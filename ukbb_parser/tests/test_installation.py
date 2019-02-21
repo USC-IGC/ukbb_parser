7b7be21c09dd9d568ba09b576d470eea0b5aa92f
azhu <ahzhoo@gmail.com>
2019-02-20 17:06:54 -0800

    test: fixes, attempt N+2

--- ukbb_parser/tests/test_installation.py
+++ ukbb_parser/tests/test_installation.py
@@ -1,6 +1,7 @@
 def main():
     try:
         import ukbb_parser.scripts.demo_conversion as dc
+        print("Looking good so far")
     except ImportError:
         from ukbb_parser.ukbb_parser import scripts
         print(scripts.__file__)
